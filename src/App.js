7511584f29342b0fbf3f5eec03f2f3d9d13eaf66
foshanxiaoyu <1422054416@qq.com>
2022-10-13 14:10:17 +0800

    建板，出色数组

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const width = 8;
+const candyColors = ["blue", "green", "orange", "red", "purple", "yellow"];
+
+const App = () => {
+  //当前数据状态初始
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+  // 建板
+  const createBoard = () => {
+    const randdomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)]; // 随机取值
+      randdomColorArrangement.push(randomColor);
+    }
+    // console.log(randdomColorArrangement);
+    setCurrentColorArrangement(randdomColorArrangement);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+  console.log(currentColorArrangement);
+
+  return <div className="app">
+    <div className="game">
+        {currentColorArrangement.map((candyColor,index)=>(
+            <img 
+                key={index}
+                style={{background:candyColor}}
+                alt={candyColor}
+            />)
+          )
+        }
     </div>
-  );
-}
+  </div>;
+};
 
 export default App;
